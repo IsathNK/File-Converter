96b870e25d15875abf0ff81f6d8986d2f2abec00
Isath Nenuka <gvisath@gmail.com>
2022-07-26 12:19:36 +0530

    Updated UI

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 #Imports
 from PIL import Image
 import os
-import re
+from termcolor import colored
+
+
+
+print(colored("### Developed And Maintained By Isath Nenuka ###", "green"))
+
+
+print(colored("Only JPG to PNG And Vice versa Are Supported For Now.", "red"))
 
 #Convert PNG to JPG
 def png_jpg(name , replace):
@@ -19,8 +26,8 @@ for files in dir:
     print(files)
 
 #User Inputs
-name = input("Enter File Name: ")
-replace = input("Enter Name For New File: ")
+name = input("Enter File Name "+ colored("(With Extension) :" , "blue"))
+replace = input("Enter Name For New File " + colored("(Without Extension) :" , "blue"))
 
 result = name.split('.')
 
